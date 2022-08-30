783db05cac700b6fa57a1fc915d88590d802f390
Jordan Halfman <jordanhalfman@gmail.com>
2022-08-29 17:43:07 -0700

    commit tree 7357032f037b53aeed155e019e27ebaa5e1d8a55

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add one, two
+    one + two
+end
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+
+    number / 2
+end
